ec5a151e034eec95913a94fb6bb503e781964041
samashtar <sam.ashtar@flatironschool.com>
2018-10-30 14:27:34 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import LoginForm from "./components/LoginForm";
 import TwitterMessage from "./components/TwitterMessage";
 
 class App extends Component {
 
-  login = ({ username, password }) => {
+  login = ({username, password}) => {
+    debugger
     console.log(`Logging in ${username} with password ${password}`);
   };
 
   render() {
-    return (
-      <div>
+    return (<div>
+      <h1>
+        <pre>LoginForm</pre>
+      </h1>
+      <LoginForm onSubmit={this.login}/>
 
-        <h1>
-          <pre>LoginForm</pre>
-        </h1>
-        <LoginForm onSubmit={this.login} />
+      <h1>
+        <pre>TwitterMessage</pre>
+      </h1>
+      <TwitterMessage maxChars={140}/>
 
-        <h1>
-          <pre>TwitterMessage</pre>
-        </h1>
-        <TwitterMessage maxChars={140} />
-
-
-
-      </div>
-    )
+    </div>)
   }
 }
 
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,32 +1,45 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
 
-    this.state = {};
+  handleUsername = (event) => {
+    this.setState({username: event.target.value})
+  }
+  handlePassword = (event) => {
+    this.setState({password: event.target.value})
+  }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    if (!this.state.username || !this.state.password) 
+      return
+    this.props.onSubmit(this.state)
   }
 
   render() {
-    return (
-      <form>
-        <div>
-          <label>
-            Username
-            <input id="username" name="username" type="text" />
-          </label>
-        </div>
-        <div>
-          <label>
-            Password
-            <input id="password" name="password" type="password" />
-          </label>
-        </div>
-        <div>
-          <button type="submit">Log in</button>
-        </div>
-      </form>
-    );
+    return (<form onSubmit={this.handleSubmit}>
+      <div>
+        <label>
+          Username
+          <input id="username" name="username" value={this.state.username} onChange={this.handleUsername} type="text"/>
+        </label>
+      </div>
+      <div>
+        <label>
+          Password
+          <input id="password" name="password" value={this.state.password} onChange={this.handlePassword} type="password"/>
+        </label>
+      </div>
+      <div>
+        <button type="submit">Log in</button>
+      </div>
+    </form>);
   }
 }
 
